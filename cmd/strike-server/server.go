335b126117eb9047e103f4a946e8f7e5f81edf39
Johnny <glynn_j@icloud.com>
2024-12-17 18:24:09 +0800

    Implemented KeyHandshake() on Server
    -Implemented the KeyHandshake RPC on the server, it takes the ClientInit
    and prints the Username and public key to the servers stream.

--- cmd/strike-server/server.go
+++ cmd/strike-server/server.go
@@ -1,7 +1,9 @@
 package strike_server
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
 
 	pb "github.com/JohnnyGlynn/strike/msgdef/message"
@@ -35,7 +37,15 @@ func (s *strikeServer) Login(ctx context.Context, clientLogin *pb.ClientLogin) (
 }
 
 func (s *strikeServer) KeyHandshake(ctx context.Context, clientinit *pb.ClientInit) (*pb.Stamp, error) {
-	return &pb.Stamp{}, nil
+	//TODO: Create a "user" in the DB
+	fmt.Printf("New User signup: %s\n", clientinit.Uname)
+	fmt.Printf("%v's Public Key: %v\n", clientinit.Uname, clientinit.PublicKey)
+
+	//TODO: Change message type to support keys not ints
+	var keyAsInt int32
+	binary.Read(bytes.NewReader(clientinit.PublicKey), binary.BigEndian, &keyAsInt)
+
+	return &pb.Stamp{KeyUsed: keyAsInt}, nil
 }
 
 func InitServer() *strikeServer {
