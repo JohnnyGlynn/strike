5f6741b1d90fbbfea2576ec3c9ed3841980abe53
Johnny <glynn_j@icloud.com>
2025-02-20 22:53:19 +0800

    Implemented Shared Secret Computation
    Added a function that will compute the shared secret between the clients
    private Cruve25519 key, and their recieved public Curve25519 key.
    
    Added signature for the public Curve key sent in a key exchange request,
    and added the relevant protobuf definition so multiple signatures (nonce
    & public Curve25519 key) can be sent in an array.
    
    - Function for computing shared secret using Curve25519 keys
    - Ability to send multiple signatures in a KeyExchangeRequest added.
    - Protobuf definiton to send multiple signatures

--- internal/client/keyexchange.go
+++ internal/client/keyexchange.go
@@ -2,8 +2,10 @@ package client
 
 import (
 	"context"
+	"crypto/ecdh"
 	"crypto/ed25519"
 	"crypto/rand"
+	"fmt"
 	"log"
 
 	pb "github.com/JohnnyGlynn/strike/msgdef/message"
@@ -17,15 +19,18 @@ func InitiateKeyExchange(ctx context.Context, c pb.StrikeClient, username string
 		log.Fatalf("Error generating nonce: %v", err)
 	}
 
-	// sign nonce
-	signature := ed25519.Sign(ed25519.PrivateKey(privateEDKey), nonce)
+	// signatures
+	nonceSig := ed25519.Sign(ed25519.PrivateKey(privateEDKey), nonce)
+	publicKeySig := ed25519.Sign(ed25519.PrivateKey(privateEDKey), publicCurveKey)
+
+	sigs := [][]byte{nonceSig, publicKeySig}
 
 	exchangeInfo := pb.KeyExchangeRequest{
-		ChatId:           chat.Id,
-		SenderUserId:     username, // TODO: Users need ID's
-		CurvePublicKey:   publicCurveKey,
-		Nonce:            nonce,
-		Ed25519Signature: signature, // Sign more than nonce?
+		ChatId:         chat.Id,
+		SenderUserId:   username, // TODO: Users need ID's
+		CurvePublicKey: publicCurveKey,
+		Nonce:          nonce,
+		Signatures:     sigs,
 	}
 
 	resp, err := c.InitiateKeyExchange(ctx, &exchangeInfo)
@@ -33,4 +38,24 @@ func InitiateKeyExchange(ctx context.Context, c pb.StrikeClient, username string
 		log.Fatalf("Error initiating key exchange: %v", err)
 	}
 	return resp
+}
+
+func ComputeSharedSecret(privateCurveKey []byte, inboundKeyExchange *pb.KeyExchangeResponse) ([]byte, error) {
+	// Validate our keys from []byte``
+	private, err := ecdh.X25519().NewPrivateKey(privateCurveKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate key: %v", err)
+	}
+
+	public, err := ecdh.X25519().NewPublicKey(inboundKeyExchange.CurvePublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate key: %v", err)
+	}
+
+	sharedSecret, err := private.ECDH(public)
+	if err != nil {
+		return nil, fmt.Errorf("failed to compute shared secret: %v", err)
+	}
+
+	return sharedSecret, nil
 }
