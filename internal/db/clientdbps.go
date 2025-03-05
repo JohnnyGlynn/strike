6f9cf71583c0c52f1ede69788404295d61aa1a4c
Johnny <glynn_j@icloud.com>
2025-03-05 21:49:34 +0800

    Modified Prepared statements
    Added a prepared statement to modify the chat state. Modified the
    GetMessages statement to return messages in order.

--- internal/db/clientdbps.go
+++ internal/db/clientdbps.go
@@ -8,14 +8,15 @@ import (
 )
 
 type ClientDB struct {
-	GetPublicKeys    string
-	GetUserId        string
-	CreatePublicKeys string
-	SaltMine         string
-	CreateChat       string
-	SaveMessage      string
-	GetChat          string
-	GetMessages      string
+	GetUserKeys     string
+	GetUserId       string
+	SaveUserKeys    string
+	SaltMine        string
+	CreateChat      string
+	UpdateChatState string
+	SaveMessage     string
+	GetChat         string
+	GetMessages     string
 }
 
 func PreparedClientStatements(ctx context.Context, dbpool *pgxpool.Pool) (*ClientDB, error) {
@@ -27,33 +28,28 @@ func PreparedClientStatements(ctx context.Context, dbpool *pgxpool.Pool) (*Clien
 	defer poolConnection.Release()
 
 	statements := &ClientDB{
-		GetPublicKeys:    "getPublicKeys",
-		GetUserId:        "getUserId",
-		CreatePublicKeys: "createPublicKeys",
-		SaltMine:         "saltMine",
-		CreateChat:       "createChat",
-		SaveMessage:      "saveMessage",
-		GetChat:          "getChat",
-		GetMessages:      "getMessages",
-	}
-
-	// salt retrieval
-	if _, err := poolConnection.Conn().Prepare(ctx, statements.SaltMine, "SELECT salt FROM users WHERE username = $1"); err != nil {
-		return nil, err
+		GetUserKeys:     "getUserKeys",
+		GetUserId:       "getUserId",
+		SaveUserKeys:    "saveUserKeys",
+		CreateChat:      "createChat",
+		GetChat:         "getChat",
+		UpdateChatState: "UpdateChatState",
+		SaveMessage:     "saveMessage",
+		GetMessages:     "getMessages",
 	}
 
 	// Get keys from key table
-	if _, err := poolConnection.Conn().Prepare(ctx, statements.GetPublicKeys, "SELECT key.encryption_public_key, key.signing_public_key FROM user_keys key JOIN users u ON key.user_id = u.id WHERE u.username = $1;"); err != nil {
+	if _, err := poolConnection.Conn().Prepare(ctx, statements.GetUserKeys, "SELECT key.encryption_public_key, key.signing_public_key FROM user_keys key JOIN users u ON key.user_id = u.id WHERE u.username = $1;"); err != nil {
 		return nil, err
 	}
 
-	// Get User Id 
+	// Get User Id
 	if _, err := poolConnection.Conn().Prepare(ctx, statements.GetUserId, "SELECT id FROM users WHERE username = $1;"); err != nil {
 		return nil, err
 	}
 
 	// Insert Users keys - Save external user keys
-	if _, err := poolConnection.Conn().Prepare(ctx, statements.CreatePublicKeys, "INSERT INTO user_keys (user_id, encryption_public_key, signing_public_key) VALUES ($1, $2, $3)"); err != nil {
+	if _, err := poolConnection.Conn().Prepare(ctx, statements.SaveUserKeys, "INSERT INTO user_keys (user_id, encryption_public_key, signing_public_key) VALUES ($1, $2, $3)"); err != nil {
 		return nil, err
 	}
 
@@ -62,18 +58,23 @@ func PreparedClientStatements(ctx context.Context, dbpool *pgxpool.Pool) (*Clien
 		return nil, err
 	}
 
-	// Insert Message - Insert message bound by chat
-	if _, err := poolConnection.Conn().Prepare(ctx, statements.SaveMessage, "INSERT INTO messages (id, chat_id, sender, content) VALUES ($1, $2, $3, $4)"); err != nil {
-		return nil, err
-	}
-
 	// get chat
 	if _, err := poolConnection.Conn().Prepare(ctx, statements.GetChat, "SELECT * FROM chats WHERE chat_id = $1"); err != nil {
 		return nil, err
 	}
 
+	// Updated Chat state
+	if _, err := poolConnection.Conn().Prepare(ctx, statements.UpdateChatState, "UPDATE chats SET state = $1 WHERE chat_id = $2"); err != nil {
+		return nil, err
+	}
+
+	// Insert Message - Insert message bound by chat
+	if _, err := poolConnection.Conn().Prepare(ctx, statements.SaveMessage, "INSERT INTO messages (id, chat_id, sender, content) VALUES ($1, $2, $3, $4)"); err != nil {
+		return nil, err
+	}
+
 	// get all messages
-	if _, err := poolConnection.Conn().Prepare(ctx, statements.GetMessages, "SELECT * FROM messages WHERE chat_id = $1"); err != nil {
+	if _, err := poolConnection.Conn().Prepare(ctx, statements.GetMessages, "SELECT * FROM messages WHERE chat_id = $1 ORDER BY sent_at ASC, id ASC;"); err != nil {
 		return nil, err
 	}
 
