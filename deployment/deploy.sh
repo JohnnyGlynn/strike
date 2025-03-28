a276dfc6ad41141380abf4e6e6a02bd632960fc8
Johnny <glynn_j@icloud.com>
2025-03-28 22:59:06 +0800

    Added basic deploy shell script
    Makefile is helpful but probably needs an overhaul, deploy.sh will
    teardown and rebuild/run all containers needed for the Strike Messaging
    Service.

---
+++ deployment/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#TODO: create network if not exist
+#podman network create strikenw
+
+podman stop strike_server strike_db
+
+# Clean images
+podman rm strike_client strike_client1 strike_server strike_db
+
+podman build -t strike_db -f deployment/StrikeDatabase.ContainerFile .
+
+podman run -d --env-file=./config/env.db --name strike_db --network=strikenw -p 5432:5432 localhost/strike_db:latest
+
+podman build -t strike_server -f deployment/StrikeServer.ContainerFile .
+
+podman run -d --env-file=./config/env.server -v ~/.strike-server/:/home/strike-server/ --name strike_server --network=strikenw -p 8080:8080 localhost/strike_server:latest
+
+podman build -t strike_client -f deployment/StrikeClient.ContainerFile .
+
+#only one run container
+podman run -it --env-file=./config/env.client -v ~/.strike-keys/:/home/strike-client/ -v ~/.strike-server/strike_server.crt:/home/strike-client/strike_server.crt --name strike_client --network=strikenw localhost/strike_client:latest
+
+#TODO: restart?
+# $(CONTAINER_RUNTIME) start -a strike_server
+
