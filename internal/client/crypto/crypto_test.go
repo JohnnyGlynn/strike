59650ecbec97091dafc9535bf34b2d373996fbef
Johnny <glynn_j@icloud.com>
2025-08-15 23:58:35 +0800

    Added table driven tests and new case.
    Updated TestDeriveKeys to include a table, adding another case for an
    empty secret.

--- internal/client/crypto/crypto_test.go
+++ internal/client/crypto/crypto_test.go
@@ -5,39 +5,67 @@ import (
 	"testing"
 
 	"github.com/JohnnyGlynn/strike/internal/client/types"
+	// "github.com/JohnnyGlynn/strike/internal/shared"
 )
 
+func TestDeriveKeys(t *testing.T) {
+	type tparams struct {
+		keylen int
+		error  bool
+	}
 
-func TestDeriveKeys (t *testing.T) {
-  sharedsecret := []byte("shared-secret")
-  c := types.Client{
-    State: &types.ClientState{
-      Cache: types.Cache{},
-    },
-  }
+	cases := map[string]struct {
+		c, c2        *types.Client
+		sharedsecret []byte
+		tparams      tparams
+	}{
+		"equal-output": {
+			c:            &types.Client{State: &types.ClientState{Cache: types.Cache{}}},
+			c2:           &types.Client{State: &types.ClientState{Cache: types.Cache{}}},
+			sharedsecret: []byte("shared-secret"),
+			tparams:      tparams{keylen: 32, error: false},
+		},
+		"no-secret": {
+			c:            &types.Client{State: &types.ClientState{Cache: types.Cache{}}},
+			c2:           nil,
+			sharedsecret: []byte(""),
+			tparams:      tparams{error: true},
+		},
+	}
 
-  enc, hmac, err := DeriveKeys(&c, sharedsecret)
-  if err != nil {
-    t.Fatalf("DeriveKeys failed: %v", err)
-  }
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			enc, hmac, err := DeriveKeys(tc.c, tc.sharedsecret)
+			if tc.tparams.error {
+				if err == nil {
+					t.Fatal("error: no error")
+				}
+				return
+			}
 
-  c2 := types.Client{
-    State: &types.ClientState{
-      Cache: types.Cache{},
-    },
-  }
+			if err != nil {
+				t.Fatalf("derive error 1: %v", err)
+			}
 
-  enc2, hmac2, err := DeriveKeys(&c2, sharedsecret)
-  if err != nil {
-    t.Fatalf("DeriveKeys failed: %v", err)//fail now
-  }
+			if len(enc) != tc.tparams.keylen || len(hmac) != tc.tparams.keylen {
+				t.Error("keys inconsistent: length not 32 byte")
+			}
 
-  if !bytes.Equal(enc, enc2) || !bytes.Equal(hmac, hmac2) {
-    t.Error("keys inconsistent: bytes not equal")
-  }
+			if tc.c2 != nil {
+				enc2, hmac2, err2 := DeriveKeys(tc.c2, tc.sharedsecret)
 
-  if len(enc) != 32 || len(enc2) != 32 {
-    t.Error("keys inconsistent: length not 32 byte")
-  } 
+				if err2 == nil {
+					t.Fatalf("derive error 2: %v", err2)
+				}
 
+				if !bytes.Equal(enc, enc2) || !bytes.Equal(hmac, hmac2) {
+					t.Error("keys inconsistent: bytes not equal")
+				}
+
+			}
+
+		})
+	}
 }
