c3e4a8b04b1408b12ae395c6008f74dad5807297
Johnny <glynn_j@icloud.com>
2025-09-14 23:58:20 +0800

    Added skeleton fo InitID()
    Expaning the identity capabilities of the server will require a look a
    how keys are generated/loaded.
    
    Added basic skeleton logic to check for an id json config and generate
    it and required keys if not found.

--- internal/server/identity.go
+++ internal/server/identity.go
@@ -3,20 +3,71 @@ package server
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"os"
 
-	// "github.com/JohnnyGlynn/strike/internal/config"
+	"github.com/JohnnyGlynn/strike/internal/config"
+	"github.com/JohnnyGlynn/strike/internal/keys"
 )
 
 const idFile = "identity.json"
 
 type Identity struct {
-  ID         string `json:"id"`
-  Name string `json:"name"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
-// func InitID(svrCfg config.ServerConfig) (*Identity, error) {
+func InitID(svrCfg config.ServerConfig, idCfg string) (*Identity, error) {
 
-// }
+	//If an existing id config has been created
+	if _, err := os.Stat(idCfg); err == nil {
+		file, err := os.ReadFile(idCfg)
+		if err != nil {
+			return nil, err
+		}
+
+		var id Identity
+		if err := json.Unmarshal(file, &id); err != nil {
+			return nil, err
+		}
+		return &id, nil
+	}
+
+	//TODO: first run
+	//no id config, generate keys and cert
+
+	//TODO: Configurable paths
+	err := keys.GenerateServerKeysAndCert()
+	if err != nil {
+		return nil, err
+	}
+
+	keyBytes, err := keys.GetKeyFromPath(svrCfg.SigningPublicKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	fingerprint := DeriveServerID(keyBytes)
+
+	id := &Identity{
+		ID:   fingerprint,
+		Name: "MAKE-CONFIGURABLE",
+	}
+
+	writes, err := json.Marshal(id)
+	if err != nil {
+		return nil, err
+	}
+
+	//Json path
+	err = os.WriteFile(idCfg, writes, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	return id, nil
+
+}
 
 func DeriveServerID(pub []byte) string {
 	di := sha256.Sum256(pub)
